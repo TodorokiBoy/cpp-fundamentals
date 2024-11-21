a18d40805bf3034ab120a833a9e45181a17eaa9f
TodorokiBoy <piotrekzywczok@gmail.com>
2024-11-21 20:40:56 +0100

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0) {
+        return std::abs(lhs + rhs);
+    }
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+    while (rhs != 0) {
+        int temp = lhs;
+        lhs = rhs;
+        rhs = temp % rhs;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    }
+    return std::abs(lhs / NWD(lhs, rhs) * rhs);
 }
