46c3e88db8db47f5e271533b7523f027354566fd
TodorokiBoy <piotrekzywczok@gmail.com>
2024-11-25 17:02:16 +0100

    adding works

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,33 @@
+#include "vectorFunctions.hpp"
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> result;
+    for (int i = 0; i < count; i++) {
+        result.push_back(std::make_shared<int>(i));
+    }
+
+    return result;
+}
+
+void add10(std::vector<std::shared_ptr<int>>& vec) {
+    std::for_each(vec.begin(), vec.end(), [](auto el) {if (el != nullptr) *el += 10; });
+}
+
+void sub10(int* ptr) {
+    if (ptr != nullptr) {
+        *ptr -= 10;
+    }
+}
+
+void sub10(std::vector<std::shared_ptr<int>>& vec) {
+    std::for_each(vec.begin(), vec.end(), [](auto el) { sub10(el.get()); });
+}
+
+void print(const std::vector<std::shared_ptr<int>>& vec) {
+    std::for_each(vec.begin(), vec.end(), [](auto el) { std::cout << *el << " "; });
+    std::cout << "\n";
+}
